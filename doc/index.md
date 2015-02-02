aa7eee5daf86992cb9acfda4816b3a87b9685d5f
gnzlbg <gonzalobg88@gmail.com>
2015-02-02 13:18:35 +0100

    Updated documentation, readme, and enabled travis
    - moved the tuple_cat example into the example directory
    - commented the tuple_cat example
    - added examples to the doxygen documentation
    - added small tutorial sections for metafunctions, metafunction classes,
      type lists, and metafunction composition.
    - enabled travis (updated also CMakeLists.txt with -std=c++11)

--- doc/index.md
+++ doc/index.md
@@ -3,38 +3,108 @@ User Manual       {#mainpage}
 
 \tableofcontents
 
-\section tutorial-preface Preface
-
 --------------------------------------------
-*Meta: a tiny metaprogramming library* is the evolution of a minimal useful set
-of primitives for manipulating lists of types introduced by Eric Niebler in
-[this blog post](http://ericniebler.com/2014/11/13/tiny-metaprogramming-library/)
-and used throught his
-[range-v3 library](https://github.com/ericniebler/range-v3).
+*Meta* is a C++14 tiny metaprogramming library developed by
+[Eric Niebler](https://github.com/ericniebler).
 
-All of the source code in this project was written by
-[Eric Niebler](https://github.com/ericniebler) as a set of utilities within his
-[range-v3 library](https://github.com/ericniebler/range-v3) and is under the
-Boost Software License (see the attached LICENSE.md) file.
-
-I have made some minor tweaks to the comments, examples, test, and
-documentation, and hopefully I haven't introduced any errors. If you find an
-error or a bug please fill an issue, these are probably mine.
-
-\subsection tutorial-installation Installation
-
---------------------------------------------
-This library is header-only. You can get its source code from its
-[github repository](https://github.com/gnzlbg/meta). To compile with
-meta, you have to:
+It is released under the Boost Software License and it is header only, that is,
+to compile with meta you just have to:
 
 ~~~~~~~{.cpp}
 #include <meta/meta.hpp>
 ~~~~~~~
 
-\section tutorial-quick-start Quick Start
+--------------------------------------------
+
+The documentation of *Meta* is currently scarce. The best resources are the
+<a href="group__meta.html">Reference</a> and the
+<a href="examples.html">Examples</a>.
+
+As a motivation and introduction to the library you can read
+[Eric's original blog post](http://ericniebler.com/2014/11/13/tiny-metaprogramming-library/),
+but please keep in mind that the library has evolved quite a bit since then.
 
 --------------------------------------------
 
-For a quick start see Eric Niebler's blog post:
-[A tiny metaprogramming library]([this blog post](http://ericniebler.com/2014/11/13/tiny-metaprogramming-library/)).
+## Tutorial
+
+The tutorial begins with a brief introduction to metafunctions, metafunction
+classes, and type lists. Then it moves to metafunction composition and
+currying. Finally, it covers type list algorithms and algorithms for working on
+integer sequences.
+
+### Metafunction
+
+Metafunctions are functions that operate on types. For example,
+
+\snippet example/tutorial_snippets.cpp meta_function0
+
+is a metafunction taking an arbitrary number of types that always returns
+void. The return value of a meta function is obtained from a nested type alias
+called (by convention) `type`. All of the metafunctions in the C++11 standard
+library follow this convention. C++11 template aliases allow us to write:
+
+\snippet example/tutorial_snippets.cpp meta_function1
+
+and thus to omit the `typename X::type` when calling a metafunction. C++14
+standard library provides `_t` metafunction aliases for all the metafunctions in
+the standard library.
+
+*Meta* provides `meta::eval<T>`, which evaluates the metafunction `T` by
+ returning the tested `T::type` alias. This allows metafunction aliases to be
+ written as follows:
+
+\snippet example/tutorial_snippets.cpp meta_function2
+
+### Metafunction Class
+
+A Metafunction Class is a form of metafunction suitable for higher-order
+metaprogramming. It is a class with a nested metafunction called (by convention)
+`apply`:
+
+\snippet example/tutorial_snippets.cpp meta_function_class0
+
+*Meta* provides the `meta::apply<F, Args...>` metafunction that evaluates the
+ metafunction class `F` with the arguments `Args`:
+
+\snippet example/tutorial_snippets.cpp meta_function_class1
+
+To turn a metafunction into a metafunction class *Meta* provides the
+`meta::quote<F>` metafunction:
+
+\snippet example/tutorial_snippets.cpp meta_function_class2
+
+Note that in the first case we create a metafunction class that will evaluate
+the metafunction through apply, while in the second case we create a
+metafunction class containing the already evaluated result.
+
+### Type lists
+
+A list of types `Ts...` can be stored in the metafunction
+`meta::list<Ts...>`. It provides a O(1) member function
+`meta::list::size()` that returns the size of the list.
+
+You can concatenate multiple lists using `meta::concat<Lists...>`:
+
+\snippet example/tutorial_snippets.cpp type_list0
+
+To flatten a list of lists, `meta::join<ListOfLists>` is provided:
+
+\snippet example/tutorial_snippets.cpp type_list1
+
+To convert other type sequences into a `meta::List`, the utility metafunction
+`meta::as_list<Sequence>` is provided. For example:
+
+\snippet example/tutorial_snippets.cpp type_list2
+
+> TODO: specify how to extend as_list to work on custom type sequences
+
+### Composition
+
+Multiple metafunctions classes can be composed into a single metafunction using
+`meta::compose<F0, F1, ..., FN>`, which returns a new metafunction class that
+performs `FN(... (F1(F0(Args...)) )`:
+
+\snippet example/tutorial_snippets.cpp composition0
+
+### Currying
---
+++ example/tuple_cat.cpp
@@ -0,0 +1,133 @@
+/// \file tuple_cat.cpp Example implementation of tuple cat using meta
+///
+/// Meta: a tiny metaprogramming library
+///
+///  Copyright Eric Niebler 2013-2015
+///
+///  Use, modification and distribution is subject to the
+///  Boost Software License, Version 1.0. (See accompanying
+///  file LICENSE_1_0.txt or copy at
+///  http://www.boost.org/LICENSE_1_0.txt)
+///
+/// Project home: https://github.com/ericniebler/meta
+///
+/// Acknowledgements: Thanks Stephan T. Lavavej for spreading the word about
+///                   tuple_cat, and the idea of bundling the tuples into a
+///                   tuple
+///                   of tuples and then using 2-dimensional indexing.
+#include <tuple>
+#include <meta/meta.hpp>
+
+using namespace meta;
+
+template <class T> struct dump;
+
+// Tuple cat is a function that takes N tuples and glues them together into one.
+//
+// The main idea behind the implementation is to take all the tuples and build
+// them up into a tuple of tuples. Since tuples are random-access, we can index
+// into this 2-dimensional array of tuples with (i,j) coordinates, where i is
+// the tuple index, and j the index of an element within the tuple i.
+
+// This helper function takes a tuple of tuples (Tuples), a list of tuple
+// indicies (Is), a list of tuple element indices (Js), and the type of the
+// tuple to be returned.
+template <typename Ret, typename... Is, typename... Js, typename Tuples>
+Ret tuple_cat_helper(list<Is...>, list<Js...>, Tuples tpls)
+{
+    // Note that for each element in a tuple we have a coordinate pair (i, j),
+    // that is, the length of the lists containing the Is and the Js must be
+    // equal:
+    static_assert(sizeof...(Is) == sizeof...(Js), "");
+    // It then explodes the tuple of tuples into the return type using the
+    // coordinates (i, j) for each element:
+    return Ret{std::get<Js::value>(std::get<Is::value>(tpls))...};
+}
+
+// Now we can implement tuple cat as follows:
+// - first we compute the inner indices (the i's), that is, to which tuple each
+//   element belongs,
+// - then we compute the outer indices (the j's), that is, the position of each
+//   element within their respective tuple
+template <
+  typename... Tuples,
+  // The return type of the tuple is computed by: creating a list for the
+  // elements of each tuple: list<...>, list<...>, ... Concatenating the lists
+  // into a single list, and returning a tuple of the list elements.
+  typename Res = apply_list<quote<std::tuple>, concat<as_list<Tuples>...>>>
+Res tuple_cat(Tuples &&... tpls)
+{
+    // With sizeof we compute the # of tuples to concatenate:
+    static constexpr std::size_t N = sizeof...(Tuples);
+
+    // To compute the inner indices:
+    //
+    // - First, we make a list containing one list
+    // with the elements of each tuple:
+    using list_of_lists_of_tuple_elements = list<as_list<Tuples>...>;
+    //   that is, for 3 tuples:
+    //     - tuple0<int, float>,
+    //     - tuple1<char>, and
+    //     - tuple2<float, unsigned>,
+    //   we have: list<list<int, float>, list<char>, list<float, unsigned>.
+    //
+    // - Then, we create for each tuple, a metafunction that, when called with
+    // any type, returns the tuple index. That is, we make an index sequence
+    // from [0, N), e.g., [0, 1, 2], and transform it with the metafunction
+    // always, such that we get a list of metafunctions:
+    // [always0, always1, always2].
+    using always_tuple_index =
+      transform<as_list<make_index_sequence<N>>, quote<always>>;
+    //
+    // - Afterwards, we transform(list_of_tuples, always_tuple_indices,
+    // transform):
+    //   That is, for each list of tuple elements, e.g., list<int, float>,
+    //   we call transform again with a metafunction, that no matter what type
+    //   is passed, returns the tuple index:
+    //   transform(list<int, float>, always0)
+    //       -> list<always0(int), always0(float)>
+    //       -> list<0, 0>
+    //   For the next tuples we get a list<1>, and list<2, 2>.
+    //   This returns a list of lists of inner indices:
+    using list_of_list_of_inner_indices =
+      transform<list_of_lists_of_tuple_elements, always_tuple_index,
+                quote<transform>>;
+    //   That is, listlist<0, 0>, list<1>, list<2,2>>
+    //
+    // - Finally: we flatten the list, and get the list of inner indices:
+    using inner = join<list_of_list_of_inner_indices>; // e.g. [0, 0, 1, 2, 2]
+
+    // To compute the outer indices:
+    //
+    // - Create a list of lists of tuple elements as above
+    //
+    // - For each lists, replace the list with a list of [0, size(list)) for
+    //   each list. This is done by calling transform on the list of lists of
+    //   tuple elements, with a metafunction that gets the list size, makes an
+    //   index sequence, and returns that as a list:
+    //     f(list) = as_list(make_index_sequence(size(list)));
+    //   This produces list<list<0, 1>, list<0>, list<0, 1>>:
+    // - Finally, we flatten the result: list<0, 1, 0, 0, 1>.
+    using outer = join< // flatten:
+      // replace list of list of tuple elements, with list of indices for each
+      // tuple:
+      transform<
+        list<as_list<Tuples>...>,
+        // f(list) = as_list(make_index_sequence(size(list)));
+        compose<quote<as_list>, quote_i<std::size_t, make_index_sequence>,
+                quote<size>>>>;
+    return tuple_cat_helper<Res>(
+      inner{}, outer{}, std::forward_as_tuple(std::forward<Tuples>(tpls)...));
+}
+
+int main()
+{
+    std::tuple<int, short, long> t1;
+    std::tuple<> t2;
+    std::tuple<float, double, long double> t3;
+    std::tuple<void *, char *> t4;
+    auto x = ::tuple_cat(t1, t2, t3, t4);
+    using expected_type =
+      std::tuple<int, short, long, float, double, long double, void *, char *>;
+    static_assert(std::is_same<decltype(x), expected_type>::value, "");
+}
