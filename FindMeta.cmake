a05597c128a6d4b68510ee6cd65db0a480eb354c
gnzlbg <gonzalobg88@gmail.com>
2015-02-20 17:20:03 +0100

    update FindMeta.cmake to that of range-v3

--- FindMeta.cmake
+++ FindMeta.cmake
@@ -1,23 +1,25 @@
 # Find the Meta include directory
 # The following variables are set if Meta is found.
 #  Meta_FOUND        - True when the Meta include directory is found.
-#  Meta_INCLUDE_DIRS - The path to where the poco include files are.
+#  Meta_INCLUDE_DIR  - The path to where the meta include files are.
 # If Meta is not found, Meta_FOUND is set to false.
 
 find_package(PkgConfig)
 
-find_path(META_INCLUDE_DIR
-  NAMES meta/meta.hpp 
-  DOC "Meta library header files"
-)
+if(NOT EXISTS "${Meta_INCLUDE_DIR}")
+  find_path(Meta_INCLUDE_DIR
+    NAMES meta/meta.hpp 
+    DOC "Meta library header files"
+    )
+endif()
 
-if(EXISTS "${META_INCLUDE_DIR}")
+if(EXISTS "${Meta_INCLUDE_DIR}")
   include(FindPackageHandleStandardArgs)
-  mark_as_advanced(META_INCLUDE_DIR)
+  mark_as_advanced(Meta_INCLUDE_DIR)
 else()
   include(ExternalProject)
   ExternalProject_Add(meta
-    GIT_REPOSITORY git@github.com:ericniebler/meta.git
+    GIT_REPOSITORY https://github.com/ericniebler/meta.git
     TIMEOUT 5
     CMAKE_ARGS -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS}
     PREFIX "${CMAKE_CURRENT_BINARY_DIR}"
@@ -26,7 +28,11 @@ else()
   
   # Specify include dir
   ExternalProject_Get_Property(meta source_dir)
-  set(META_INCLUDE_DIRS ${source_dir}/include)
+  set(Meta_INCLUDE_DIR ${source_dir}/include)
 endif()
 
-set(Meta_FOUND 1)
+if(EXISTS "${Meta_INCLUDE_DIR}")
+  set(Meta_FOUND 1)
+else()
+  set(Meta_FOUND 0)
+endif()
