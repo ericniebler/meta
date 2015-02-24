afd19a2185cd559a716e83ecd0ec5eb85c9f85fa
Eric Niebler <eric@boostpro.com>
2015-02-23 17:37:11 -0800

    add missing inline namespace in the workaround header

--- include/meta/meta_libcxx_workaround.hpp
+++ include/meta/meta_libcxx_workaround.hpp
@@ -67,113 +67,118 @@ _LIBCPP_END_NAMESPACE_STD
 
 namespace meta
 {
-    namespace detail
+    inline namespace v1
     {
-        template <typename T, typename A = std::allocator<T>>
-        using std_list = std::list<T, A>;
-        template <typename T, typename A = std::allocator<T>>
-        using std_forward_list = std::forward_list<T, A>;
-        template <typename T, typename A = std::allocator<T>>
-        using std_vector = std::vector<T, A>;
-        template <typename T, typename A = std::allocator<T>>
-        using std_deque = std::deque<T, A>;
-        template <typename T, typename C = std::char_traits<T>, typename A = std::allocator<T>>
-        using std_basic_string = std::basic_string<T, C, A>;
-        template <typename K, typename V, typename C = std::less<K>,
-                  typename A = std::allocator<std::pair<K const, V>>>
-        using std_map = std::map<K, V, C, A>;
-        template <typename K, typename V, typename C = std::less<K>,
-                  typename A = std::allocator<std::pair<K const, V>>>
-        using std_multimap = std::multimap<K, V, C, A>;
-        template <typename K, typename C = std::less<K>, typename A = std::allocator<K>>
-        using std_set = std::set<K, C, A>;
-        template <typename K, typename C = std::less<K>, typename A = std::allocator<K>>
-        using std_multiset = std::multiset<K, C, A>;
-        template <typename K, typename V, typename H = std::hash<K>, typename C = std::equal_to<K>,
-                  typename A = std::allocator<std::pair<K const, V>>>
-        using std_unordered_map = std::unordered_map<K, V, H, C, A>;
-        template <typename K, typename V, typename H = std::hash<K>, typename C = std::equal_to<K>,
-                  typename A = std::allocator<std::pair<K const, V>>>
-        using std_unordered_multimap = std::unordered_multimap<K, V, H, C, A>;
-        template <typename K, typename H = std::hash<K>, typename C = std::equal_to<K>,
-                  typename A = std::allocator<K>>
-        using std_unordered_set = std::unordered_set<K, H, C, A>;
-        template <typename K, typename H = std::hash<K>, typename C = std::equal_to<K>,
-                  typename A = std::allocator<K>>
-        using std_unordered_multiset = std::unordered_multiset<K, H, C, A>;
-        template <typename T, typename C = std_deque<T>>
-        using std_queue = std::queue<T, C>;
-        template <typename T, typename C = std_vector<T>,
-                  class D = std::less<typename C::value_type>>
-        using std_priority_queue = std::priority_queue<T, C, D>;
-        template <typename T, typename C = std_deque<T>>
-        using std_stack = std::stack<T, C>;
-    }
+        namespace detail
+        {
+            template <typename T, typename A = std::allocator<T>>
+            using std_list = std::list<T, A>;
+            template <typename T, typename A = std::allocator<T>>
+            using std_forward_list = std::forward_list<T, A>;
+            template <typename T, typename A = std::allocator<T>>
+            using std_vector = std::vector<T, A>;
+            template <typename T, typename A = std::allocator<T>>
+            using std_deque = std::deque<T, A>;
+            template <typename T, typename C = std::char_traits<T>, typename A = std::allocator<T>>
+            using std_basic_string = std::basic_string<T, C, A>;
+            template <typename K, typename V, typename C = std::less<K>,
+                      typename A = std::allocator<std::pair<K const, V>>>
+            using std_map = std::map<K, V, C, A>;
+            template <typename K, typename V, typename C = std::less<K>,
+                      typename A = std::allocator<std::pair<K const, V>>>
+            using std_multimap = std::multimap<K, V, C, A>;
+            template <typename K, typename C = std::less<K>, typename A = std::allocator<K>>
+            using std_set = std::set<K, C, A>;
+            template <typename K, typename C = std::less<K>, typename A = std::allocator<K>>
+            using std_multiset = std::multiset<K, C, A>;
+            template <typename K, typename V, typename H = std::hash<K>,
+                      typename C = std::equal_to<K>,
+                      typename A = std::allocator<std::pair<K const, V>>>
+            using std_unordered_map = std::unordered_map<K, V, H, C, A>;
+            template <typename K, typename V, typename H = std::hash<K>,
+                      typename C = std::equal_to<K>,
+                      typename A = std::allocator<std::pair<K const, V>>>
+            using std_unordered_multimap = std::unordered_multimap<K, V, H, C, A>;
+            template <typename K, typename H = std::hash<K>, typename C = std::equal_to<K>,
+                      typename A = std::allocator<K>>
+            using std_unordered_set = std::unordered_set<K, H, C, A>;
+            template <typename K, typename H = std::hash<K>, typename C = std::equal_to<K>,
+                      typename A = std::allocator<K>>
+            using std_unordered_multiset = std::unordered_multiset<K, H, C, A>;
+            template <typename T, typename C = std_deque<T>>
+            using std_queue = std::queue<T, C>;
+            template <typename T, typename C = std_vector<T>,
+                      class D = std::less<typename C::value_type>>
+            using std_priority_queue = std::priority_queue<T, C, D>;
+            template <typename T, typename C = std_deque<T>>
+            using std_stack = std::stack<T, C>;
+        }
 
-    template <>
-    struct quote<::std::list> : quote<detail::std_list>
-    {
-    };
-    template <>
-    struct quote<::std::deque> : quote<detail::std_deque>
-    {
-    };
-    template <>
-    struct quote<::std::forward_list> : quote<detail::std_forward_list>
-    {
-    };
-    template <>
-    struct quote<::std::vector> : quote<detail::std_vector>
-    {
-    };
-    template <>
-    struct quote<::std::basic_string> : quote<detail::std_basic_string>
-    {
-    };
-    template <>
-    struct quote<::std::map> : quote<detail::std_map>
-    {
-    };
-    template <>
-    struct quote<::std::multimap> : quote<detail::std_multimap>
-    {
-    };
-    template <>
-    struct quote<::std::set> : quote<detail::std_set>
-    {
-    };
-    template <>
-    struct quote<::std::multiset> : quote<detail::std_multiset>
-    {
-    };
-    template <>
-    struct quote<::std::unordered_map> : quote<detail::std_unordered_map>
-    {
-    };
-    template <>
-    struct quote<::std::unordered_multimap> : quote<detail::std_unordered_multimap>
-    {
-    };
-    template <>
-    struct quote<::std::unordered_set> : quote<detail::std_unordered_set>
-    {
-    };
-    template <>
-    struct quote<::std::unordered_multiset> : quote<detail::std_unordered_multiset>
-    {
-    };
-    template <>
-    struct quote<::std::queue> : quote<detail::std_queue>
-    {
-    };
-    template <>
-    struct quote<::std::priority_queue> : quote<detail::std_priority_queue>
-    {
-    };
-    template <>
-    struct quote<::std::stack> : quote<detail::std_stack>
-    {
-    };
+        template <>
+        struct quote<::std::list> : quote<detail::std_list>
+        {
+        };
+        template <>
+        struct quote<::std::deque> : quote<detail::std_deque>
+        {
+        };
+        template <>
+        struct quote<::std::forward_list> : quote<detail::std_forward_list>
+        {
+        };
+        template <>
+        struct quote<::std::vector> : quote<detail::std_vector>
+        {
+        };
+        template <>
+        struct quote<::std::basic_string> : quote<detail::std_basic_string>
+        {
+        };
+        template <>
+        struct quote<::std::map> : quote<detail::std_map>
+        {
+        };
+        template <>
+        struct quote<::std::multimap> : quote<detail::std_multimap>
+        {
+        };
+        template <>
+        struct quote<::std::set> : quote<detail::std_set>
+        {
+        };
+        template <>
+        struct quote<::std::multiset> : quote<detail::std_multiset>
+        {
+        };
+        template <>
+        struct quote<::std::unordered_map> : quote<detail::std_unordered_map>
+        {
+        };
+        template <>
+        struct quote<::std::unordered_multimap> : quote<detail::std_unordered_multimap>
+        {
+        };
+        template <>
+        struct quote<::std::unordered_set> : quote<detail::std_unordered_set>
+        {
+        };
+        template <>
+        struct quote<::std::unordered_multiset> : quote<detail::std_unordered_multiset>
+        {
+        };
+        template <>
+        struct quote<::std::queue> : quote<detail::std_queue>
+        {
+        };
+        template <>
+        struct quote<::std::priority_queue> : quote<detail::std_priority_queue>
+        {
+        };
+        template <>
+        struct quote<::std::stack> : quote<detail::std_stack>
+        {
+        };
+    }
 }
 
 #endif
